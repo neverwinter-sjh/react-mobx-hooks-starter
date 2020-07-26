34f162bf856bba376697b1e7d559c2190487e822
neverwinter-sjh <neverwinter.sjh@gmail.com>
2020-07-26 17:26:30 +0900

    수정

--- src/index.js
+++ src/index.js
@@ -1,9 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import 'mobx-react-lite/batchingForReactDom'
-import {
-  BrowserRouter as Router
-} from "react-router-dom";
+import { BrowserRouter as Router } from "react-router-dom";
 import App from './App';
 
 ReactDOM.render(
